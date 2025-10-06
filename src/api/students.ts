986f20051b12f3d08800cea512beb78a52dc75b9
xiertale <magnira2012@gmail.com>
2025-10-06 15:33:57 +0700

    0

---
+++ src/api/students.ts
@@ -0,0 +1,17 @@
+import type StudentInterface from '@/types/StudentInterface';
+
+export const getStudentsApi = async (): Promise<StudentInterface[]> => {
+  try {
+    const response = await fetch(`${process.env.NEXT_PUBLIC_API}students`);
+
+    if (!response.ok) {
+      throw new Error(`Ошибка HTTP: ${response.status}${response.statusText}`);
+    }
+    const students = await response.json() as StudentInterface[];
+    return students;
+  }
+  catch (err) {
+    console.log('>>> getGroupsApi', err);
+    return [] as StudentInterface[];
+  }
+};
